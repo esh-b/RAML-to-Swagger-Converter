97b196cc7d63bb4bd19c9c42ac099c9823ad4d79
Szabolcs Gyurko <szabolcs.gyurko@tribalworldwide.co.uk>
2017-11-02 12:52:29 +0000

    Added further tests

--- raml2swagger-lib/src/test/java/org/macrulez/utils/raml2swagger/RAMLtoSwaggerTest.java
+++ raml2swagger-lib/src/test/java/org/macrulez/utils/raml2swagger/RAMLtoSwaggerTest.java
@@ -21,6 +21,21 @@ public class RAMLtoSwaggerTest {
 
     @Test
     @SuppressWarnings("unchecked")
+    public void convertToSwaggerValidJson() throws Exception {
+        String raml = IOUtils.resourceToString("/product-api.raml", Charset.forName("UTF-8"));
+        Assert.assertNotNull(raml);
+
+        String swagger = raml2Swagger.convertToSwagger(raml);
+        Assert.assertNotNull(swagger);
+
+        TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
+        Map<String, Object> json = objectMapper.readValue(swagger, typeRef);
+        Assert.assertNotNull(json);
+    }
+
+
+    @Test
+    @SuppressWarnings("unchecked")
     public void convertToSwaggerFromString() throws Exception {
         String raml = IOUtils.resourceToString("/product-api.raml", Charset.forName("UTF-8"));
         Assert.assertNotNull(raml);
@@ -41,6 +56,67 @@ public class RAMLtoSwaggerTest {
         Assert.assertTrue("Paths section should be a map", json.get("paths") instanceof Map);
         Assert.assertEquals("Paths section should have 62 keys", 62, ((Map<String, Object>)json.get("paths")).size());
     }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void checkQueryParameters() throws Exception {
+        String raml = IOUtils.resourceToString("/product-api.raml", Charset.forName("UTF-8"));
+        Assert.assertNotNull(raml);
+
+        String swagger = raml2Swagger.convertToSwagger(raml);
+        Assert.assertNotNull(swagger);
+
+        TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
+        Map<String, Object> json = objectMapper.readValue(swagger, typeRef);
+        Assert.assertNotNull(json);
+
+        Map<String, Object> paths = (Map<String, Object>)json.get("paths");
+        Map<String, Object> path = (Map<String, Object>)paths.get("/{version}/cache/clear");
+        Map<String, Object> method = (Map<String, Object>)path.get("get");
+        List<Map<String, Object>> parameters = (List<Map<String, Object>>)method.get("parameters");
+        Assert.assertEquals("There should be 4 parameters", 4, parameters.size());
+        Assert.assertEquals("Parameter type should be 'query'", "query", parameters.get(0).get("in"));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void checkPathParameters() throws Exception {
+        String raml = IOUtils.resourceToString("/product-api.raml", Charset.forName("UTF-8"));
+        Assert.assertNotNull(raml);
+
+        String swagger = raml2Swagger.convertToSwagger(raml);
+        Assert.assertNotNull(swagger);
+
+        TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
+        Map<String, Object> json = objectMapper.readValue(swagger, typeRef);
+        Assert.assertNotNull(json);
+
+        Map<String, Object> paths = (Map<String, Object>)json.get("paths");
+        Map<String, Object> path = (Map<String, Object>)paths.get("/{version}/used-families/{familyName}/{modelName}/{type}");
+        List<Map<String, Object>> parameters = (List<Map<String, Object>>)path.get("parameters");
+        Assert.assertEquals("There should be 3 parameters", 3, parameters.size());
+        Assert.assertEquals("Parameter type should be 'query'", "path", parameters.get(0).get("in"));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void checkCorrectParametersResolution() throws Exception {
+        String raml = IOUtils.resourceToString("/product-api.raml", Charset.forName("UTF-8"));
+        Assert.assertNotNull(raml);
+
+        String swagger = raml2Swagger.convertToSwagger(raml);
+        Assert.assertNotNull(swagger);
+
+        TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
+        Map<String, Object> json = objectMapper.readValue(swagger, typeRef);
+        Assert.assertNotNull(json);
+
+        Map<String, Object> paths = (Map<String, Object>)json.get("paths");
+        Map<String, Object> path = (Map<String, Object>)paths.get("/{version}/models/{externalId}/derivativeFeatureCombos/{featureList}");
+        List<Map<String, Object>> parameters = (List<Map<String, Object>>)path.get("parameters");
+        Assert.assertEquals("There should be 2 parameters", 2, parameters.size());
+    }
+
 
     @Test
     public void convertToSwaggerFromInputStream() throws Exception {
